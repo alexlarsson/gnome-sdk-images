957f1de0bf5c0dcbec96c26d906ec75faea71d15
Alexander Larsson <alexl@redhat.com>
2015-03-23 16:04:11 +0100

    Use linux-user-chroot instead of xdg-app-helper to build rpms
    This is installed already on build.gnome.org, and we don't really need
    more that it to build the SDK, so we favour the smaller setuid helper
    here.

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@
 ROOT=`pwd`/build/root
 VAR=`pwd`/build/var
 APP=`pwd`/packages
+SRC=`pwd`/
 
-HELPER=`which xdg-app-helper`
+HELPER=`which linux-user-chroot`
 
 declare -x LC_ALL=en_US.utf8
 declare -x HOME=/self
@@ -14,8 +15,11 @@ declare -x CPLUS_INCLUDE_PATH="/self/include"
 declare -x C_INCLUDE_PATH="/self/include"
 declare -x GI_TYPELIB_PATH="/self/lib/girepository-1.0"
 declare -x LDFLAGS="-L/self/lib "
-declare -x PKG_CONFIG_PATH="/self/lib/pkgconfig:/self/share/pkgconfig"
+declare -x LD_LIBRARY_PATH="/self/lib"
 declare -x PATH="/usr/bin:/self/bin"
+declare -x PKG_CONFIG_PATH="/self/lib/pkgconfig:/self/share/pkgconfig"
+declare -x XDG_CONFIG_DIRS="/self/etc/xdg:/etc/xdg"
+declare -x XDG_DATA_DIRS="/self/share:/usr/share"
 if test -d packages/.ccache; then
     declare -x PATH="/self/bin/ccache:$PATH"
 fi
@@ -23,4 +27,27 @@ unset PYTHONPATH
 unset INSTALL
 unset PERL5LIB
 
-$HELPER -f -w -W -E -a $APP -v $VAR $ROOT/usr env PATH="$PATH" "$@"
+CHROOT=`mktemp -d`
+mkdir $CHROOT/var
+mkdir $CHROOT/usr
+mkdir $CHROOT/tmp
+mkdir $CHROOT/self
+mkdir $CHROOT/proc
+mkdir $CHROOT/dev
+mkdir $CHROOT/src
+ln -s usr/lib $CHROOT/lib
+ln -s usr/bin $CHROOT/bin
+ln -s usr/sbin $CHROOT/sbin
+ln -s usr/etc $CHROOT/etc
+
+cp -a $ROOT/usr/etc/passwd $CHROOT
+cp -a $ROOT/usr/etc/group $CHROOT
+cp  /etc/passwd $ROOT/usr/etc/
+cp  /etc/group $ROOT/usr/etc/
+
+$HELPER --unshare-ipc --unshare-pid --unshare-net --mount-bind /dev /dev --mount-proc /proc --mount-bind $ROOT/usr /usr --mount-bind $VAR /var --mount-bind $APP /self  --mount-bind $SRC /src --chdir /src $CHROOT "$@"
+
+cp -a $CHROOT/passwd $ROOT/usr/etc/
+cp -a $CHROOT/group $ROOT/usr/etc/
+
+rm -rf $CHROOT
